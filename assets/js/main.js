2caddffdf080693656a1e723bde05cc21b11be37
HypoxiE <kosmaer42@gmail.com>
2025-09-08 16:54:06 +0300

    Размещена основная информация

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
-async function loadUsers() {
-  try {
-    // Запрос (замени URL на свой)
-    const response = await fetch("http://localhost:8080/leaderboard?tournament_id=3");
-    if (!response.ok) throw new Error(`HTTP ошибка: ${response.status}`);
+async function getResults() {
+  // Запрос (замени URL на свой)
+  const response = await fetch("http://localhost:8080/leaderboard?tournament_id=3");
+  if (!response.ok) throw new Error(`HTTP ошибка: ${response.status}`);
+  return await response.json()
+}
 
-    const tournament = await response.json();
+async function drawTable(tournament) {
+  try {
 
     const nameDiv = document.getElementById("tournament_name");
     nameDiv.innerHTML = `${tournament.name}`;
@@ -12,32 +14,47 @@ async function loadUsers() {
     const container = document.getElementById("leaderboard");
     container.innerHTML = ""; // очищаем перед рендером
 
-    tournament.results.forEach(user => {
+    tournament.results.forEach(result => {
+
+      let comment_content = "", streams = "";
+
+      result.metadata.forEach(data => {
+        if (data.key == "comment") {
+          comment_content = data.value
+        } else if (data.key == "streams") {
+          streams = data.value
+        }
+      });
 
       const block = document.createElement("div");
-      block.className = "user-block";
+      block.className = "result-block";
 
       const avatar = document.createElement("img");
       avatar.className = "avatar";
-      avatar.src = user.avatar_url;
+      avatar.src = result.avatar_url;
       avatar.onerror = function() {
-        this.src = 'https://cdn.discordapp.com/attachments/1399166057477963777/1413458320043540531/image.png?ex=68bea424&is=68bd52a4&hm=dd82911fa78f7c746f4b8ce34fe0c1a0f4fa541d950c5894fc0efd54d03c16d3&';
+        this.src = 'https://i.imgur.com/9iT2ogu.png';
         this.onerror = null;
       };
 
       const info = document.createElement("div");
-      info.className = "user-info";
-      const date = new Date(user.timestamp * 1000);
+      info.className = "result-info-block";
+      const date = new Date(result.timestamp * 1000);
+      //date.toLocaleString()
       // Генерация содержимого блока
       info.innerHTML = `
-        <span class="username">${user.username}</span>
-        <span class="score">Score: ${user.score}</span>
-        <span class="cost">Cost: ${user.cost}</span>
-        <span class="time">Time: ${date.toLocaleString()}</span>
+        <span class="username">${result.username}</span>
+        <span class="score">Score: ${result.score}</span>
+        <span class="cost">Cost: ${result.cost}</span>
       `;
-      
+
+      const comment = document.createElement("div");
+      comment.className = "result-comment-block";
+      comment.innerHTML = comment_content;
+
       block.appendChild(avatar);
       block.appendChild(info);
+      block.appendChild(comment);
       container.appendChild(block);
     });
 
@@ -48,4 +65,9 @@ async function loadUsers() {
   }
 }
 
-loadUsers();
+async function main() {
+  const tournament = await getResults();
+  await drawTable(tournament);
+}
+
+main();
